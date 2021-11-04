3d49161ba5c71979398e72b81b12b7a048eb3e76
William <william@mgk.cloud>
2021-11-04 11:32:09 +1100

    3/11

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-const input = document.querySelector("input[type='number']")
 
-document.querySelector("input[type='number']").addEventListener('change', (event) => {
-    document.documentElement.style.setProperty('--timing', input.value + "s");
-})
+  const { BrowserWindow, app } = require('electron');
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 280,
+      height: 280,
+      setMinimizable: false,
+     
+    })
+  
+    win.loadFile('index.html');
+    win.setAlwaysOnTop(true, 'floating');
+    win.setFullScreenable(false),
+    win.setMaximizable(false),
+    win.setResizable(true),
+    win.show();
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
